1bfe7f145f6ed61fa881592b11be2d1bc6b3e502
xxishere <320916571@qq.com>
2023-04-10 08:07:36 +0800

    add index and bootstrap

--- README.md
+++ README.md
@@ -3,5 +3,7 @@
 ## 资料
 https://spring.io/guides
 https://spring.io/guides/gs/serving-web-content/
+https://v3.bootcss.com/css/#grid
+
 ##工具
 https://git-scm.com/download
--- src/main/java/life/community/community/controller/IndexController.java
+++ src/main/java/life/community/community/controller/IndexController.java
@@ -6,12 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
 @Controller
-public class HelloController {
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name = "name") String name, Model model) {
-        model.addAttribute("name",name);
-        return "hello0";
+public class IndexController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
     }
 }
